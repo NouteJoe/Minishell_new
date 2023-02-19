aa445e8918c39155e762a7b6ad2b4ccb80596439
Joanna Muni reddi <jmuni-re@k2r4p10.42mulhouse.fr>
2023-02-19 17:21:18 +0100

    Push

--- parsing/parsing.c
+++ parsing/parsing.c
@@ -1,3 +1,4 @@
+
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
@@ -31,6 +32,7 @@ int get_cmd(t_var **shell, int i, char *cmd) {
   (*shell)->cmd_arg = ft_substr(cmd, start, i - start);
   return (i);
 }
+
 int get_space(t_var **tmp)
 {
   char *c;
@@ -42,45 +44,60 @@ int get_space(t_var **tmp)
 return(0);
 }
 
-int read_cmd_user(t_var **shell, char **tmp_env, char *cmd) {
-
+int read_cmd_user(t_var **shell, char **tmp_env, char *cmd)
+ {
+// int flag_in = 0;
+ // int flag_out = 0;
   int i = 0;
   int flag = 0;
-  int flag_in = 0;
-  int flag_out = 0;
   int flag_space = 0;
 
   t_var *tmp = ft_varnew();
-  if (is_double_quote(cmd) % 2 != 0 && is_simple_quote(cmd) % 2 != 0)
-    return (1); ///////// erreur
-  while (cmd[i]) {
+  if (is_double_quote(cmd) % 2 == 0 && is_simple_quote(cmd) % 2 == 0)
+  {
+    while (cmd[i]) 
+    {
     while (cmd[i] == ' ')
       i++;
     if (ft_strncmp(&cmd[i], "<<", 2) == 0) 
     {
      // if((*shell)->redir_input)  ???????
-      flag_in = 1;
+     // flag_in = 1;
+     if((*shell)->redir_input) 
+        printf("infile or hdoc, make your choice\n"); /////////////////
+    else
       i = here_doc(&tmp, i, cmd);
     } 
     else if (cmd[i] == '<') 
     {
-      if (flag_in)
-    // if((*shell)->redir_hdoc) possible?  ??????
+      //if (flag_in)
+     if((*shell)->redir_hdoc) 
         printf("infile or hdoc, make your choice\n"); /////////////////
-      i = input_file(&tmp, i, cmd);
+    else
+       i = input_file(&tmp, i, cmd);
     } 
     else if (ft_strncmp(&cmd[i], ">>", 2) == 0) 
     {
-      flag_out = 1;
-      // if((*shell)->redir_output)  ??
+     // flag_out = 1;
+      // if((*shell)->redir_output)  
+      if((*shell)->redir_output) 
+        printf("outfile or append, make your choice\n"); ///////// exit? or  error fonction
+        else
       i = append(&tmp, i, cmd);
     } 
     else if (cmd[i] == '>') 
     {
-      if (flag_out)
-      // if((*shell)->redir_append)    ???
+     // if (flag_out)
+       if((*shell)->redir_append)   
         printf("outfile or append, make your choice\n"); ///////// exit? or  error fonction
+        else
       i = output_file(&tmp, i, cmd);
+    }
+    else if (cmd[i] == '$')
+    {
+      i = get_variable(cmd, i, tmp_env, &tmp);
+      if (cmd[i] == ' ')
+        flag_space = 1;
     }
     else if (cmd[i] != ' ' && !flag)
      {
@@ -94,8 +111,6 @@ int read_cmd_user(t_var **shell, char **tmp_env, char *cmd) {
       i = get_string_double(&tmp, i, cmd, tmp_env);
       flag_space = 1;
     } 
-    else if (cmd[i] == '$')
-      i = get_variable(cmd, i, tmp_env, &tmp);
     else if (cmd[i] == '\'') 
     {
       i = get_string_simple(&tmp, i, cmd);
@@ -109,16 +124,20 @@ int read_cmd_user(t_var **shell, char **tmp_env, char *cmd) {
       i++;
     } 
     else
+    {
       i = get_string(&tmp, i, cmd, tmp_env);
+     if (cmd[i] == ' ')
+        flag_space = 1;
+    }
     if (cmd[i] == ' ' && flag_space) 
-    flag_space = get_space(&tmp);
-   /* {
-      char *c = ft_substr(" ", 0, 1);
-      t_list *space = ft_lstnew(c);
-      ft_lstadd_back(&tmp->string, space);
-      flag_space = 0;
-    }*/
+      flag_space = get_space(&tmp);
   }
   ft_varadd_back(shell, tmp);
-  return (1); ///////revoir return
+  }
+  else
+  {
+    printf("Error: missing quote\n");
+    return(1);
+  }
+  return (0); ///////revoir return
 }
--- parsing/string.c
+++ parsing/string.c
@@ -19,16 +19,16 @@ int get_string_double(t_var **shell, int i, char *cmd, char **tmp_env) {
   i++;
   start = i;
   while (cmd[i] != '"') {
-    if (cmd[i] == '$') {
-     
-    if ((*shell)->string->content)
-      get_space(shell);///// erreur a modifier
-        tmp = ft_lstnew((char *)ft_substr(cmd, start, i - start));
-        ft_lstadd_back(&(*shell)->string, tmp);
-      
+    if (cmd[i] == '$')
+    {
+      if ((*shell)->string && (*shell)->string->content)
+        get_space(shell);///// erreur a modifier
+      tmp = ft_lstnew((char *)ft_substr(cmd, start, i - start));
+      ft_lstadd_back(&(*shell)->string, tmp); 
       i = get_variable(cmd, i, tmp_env, shell);
       start = i;
-    } else
+    } 
+    else
       i++;
   }
   tmp = ft_lstnew((char *)ft_substr(cmd, start, i - start));
@@ -37,43 +37,58 @@ int get_string_double(t_var **shell, int i, char *cmd, char **tmp_env) {
   return (i);
 }
 
-int scan_tmp_env(char **tmp_env, char *str, int j, t_var **shell) {
+int scan_tmp_env(char **tmp_env, char *str, int j, t_var **shell)
+{
   t_list *tmp = NULL;
   char **tab = NULL;
   char *tmp2 = NULL;
   int n = 0;
 
   while (tmp_env[n]) {
-    if (ft_strncmp(str, tmp_env[n], j) == 0) {
+    if (ft_strncmp(str, tmp_env[n], j) == 0)
+     {
       tab = ft_split(tmp_env[n], '=');
       tmp2 = ft_strdup(tab[1]);
       tmp = ft_lstnew(tmp2);
       ft_lstadd_back(&(*shell)->string, tmp);
-    }
+      free(tab);
+    }  
     n++;
   }
   return (0);
+}
+void exit_statut(t_var **shell)
+{
+  t_list *number;
+  char *str;
+
+    str = malloc (sizeof(char) * 4); 
+    str = ft_itoa(g_exit_statut);
+    if ((*shell)->cmd_arg == NULL)
+      (*shell)->cmd_arg = str;
+    else 
+    { 
+      number = ft_lstnew((char *)ft_substr(str, 0, 4));
+      ft_lstadd_back(&(*shell)->string, number);
+    } 
 }
 
 int get_variable(char *cmd, int i, char **tmp_env, t_var **shell) 
 {
   int j = 0;
   char *str = NULL;
-  // char *nbr;
   int size = 0;
 
   i++;
-  /*nbr = ft_itoa(exit_statut);
-  if (cmd[i] == '?') {
-    while (nbr[j])
-      str[j] = nbr[j];
-    j++;
+  if (cmd[i] == '?') 
+  {
+    exit_statut(shell);
+    i++;
   }
-  i++;*/
   size = is_forbidden_char(size, cmd);
   str = malloc(sizeof(char) * size + 1);
   while (cmd[i] && cmd[i] != ' ' && cmd[i] != '|' && cmd[i] != '"' &&
-         cmd[i] != '\'')
+         cmd[i] != '\''&& cmd[i] != '$')
    {
     str[j] = cmd[i];
     i++;
@@ -102,31 +117,35 @@ int get_string_simple(t_var **shell, int i, char *cmd) {
 int get_string(t_var **shell, int i, char *cmd, char **tmp_env) {
   int start;
   t_list *tmp;
-  (void)tmp_env;
+  //(void)tmp_env;
 
   start = i;
   while (cmd[i] && cmd[i] != '<' && cmd[i] != '>' && cmd[i] != '\'' &&
-         cmd[i] != '"' && cmd[i] != '|') {
-    if (cmd[i] == ' ' && cmd[i + 1] == ' ') {
-      while (cmd[i] == ' ' && cmd[i + 1] == ' ')
-        i++;
+         cmd[i] != '"' && cmd[i] != '|')
+   {
+    //1 space
+      if (cmd[i] == ' ' && cmd[i + 1] == ' ') 
+       {
+       while (cmd[i] == ' ' && cmd[i + 1] == ' ')
+          i++;
+       start = i;
+       }
+      if (cmd[i] == '$')
+      {
+        if ((*shell)->string && (*shell)->string->content)
+            get_space(shell);///// erreur a modifier
+        tmp = ft_lstnew((char *)ft_substr(cmd, start, i - start));
+        ft_lstadd_back(&(*shell)->string, tmp);
+      
+      i = get_variable(cmd, i, tmp_env, shell);
       start = i;
-    }
-    while (cmd[i] && cmd[i] != '<' && cmd[i] != '>' && cmd[i] != '\'' &&
-           cmd[i] != '"' && cmd[i] != '|' && cmd[i] != ' ') {
-      if (cmd[i] == '$') {
-        /* tmp = ft_lstnew((char *)ft_substr(cmd, start, i - start));
-         ft_lstadd_back(&(*shell)->string, tmp);*/
-        // printf("test %s\n",(*shell)->string->content);
-        i = get_variable(cmd, i, tmp_env, shell);
-        start = i;
-      }
-      i++;
+     } 
+    i++;
     }
     tmp = ft_lstnew((char *)ft_substr(cmd, start, i - start));
     ft_lstadd_back(&(*shell)->string, tmp);
     start = i;
     i++;
+    return (i);
   }
-  return (i);
-}
+  
